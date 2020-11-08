dc5628ab0fdb49a2d81969b5c6e9da9a47f63d42
Nicholas Morera <morera.computer@gmail.com>
2020-11-08 17:54:00 +0100

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(`
+Nicholas Morera - Software Engineer and Composer.
+
+Based in Berlin, DE.
+
+📖 : https://github.com/rom-dos
+✉️  : morera.computer@gmail.com
+`)
